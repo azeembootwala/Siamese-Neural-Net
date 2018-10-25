a31f6c6de5723b5bfad801fdadf718a04f54f4fd
azeembootwala <azeem.bootwala21@gmail.com>
2018-10-25 19:02:24 +0200

    margin changes

--- VGG-triplet.py
+++ VGG-triplet.py
@@ -327,8 +327,8 @@ class VGG(object):
 
 
 def main():
-    lr = 1e-5
-    margin = 0.6
+    lr = 1e-4
+    margin = 0.2
     batch_size=16
     path = "../Triplet-Models/Batch_all-"+str(margin)+"_"+str(lr)
     Model = VGG([(3,64,64),(64,128,128)],[(128,256,256,256),(256,512,512,512),(512,512,512,512)],Normal(), batch_size , path,lr)
