3d4c949ecdeb5b8293516feb0c7363bad2967722
azeembootwala <azeem.bootwala21@gmail.com>
2018-11-04 19:14:42 +0100

    added histogram loss

---
+++ starter_histogram.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=triplet
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=12000
+#SBATCH --gres=gpu:1
+#SBATCH -o /home/%u/output-%j.out
+#SBATCH -e /home/%u/error-%j.err
+#SBATCH --mail-type=ALL
+
+# Small Python packages can be installed in own home directory. Not recommended$
+# cluster_requirements.txt is a text file listing the required pip packages (on$
+#pip3 install --user -r cluster_requirements.txty
+
+python3 VGG-histogram.py
